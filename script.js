8301147326665f6b7cf1050a3df9b0e16fac2e70
fslipe17 <128160248+fslipe17@users.noreply.github.com>
2023-12-04 02:13:50 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+document.getElementById('login-form').addEventListener('submit', function(event) {
+    event.preventDefault();
+
+    var username = document.getElementById('username').value;
+    var password = document.getElementById('password').value;
+
+    if (username === 'Nicole' && password === 'vida') {
+        alert('Conectado com sucesso.');
+        window.open('index2.html')
+    } else {
+        alert('Nome de usuario ou senha incorretas!!');
+    }
+});
